13378e00ef6eef5ea4b49d21f49807d687544d47
aibayanyu <aibayanyu@qq.com>
2023-04-12 21:57:02 +0800

    feat: add api

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+// 配置拦截规则
+const options = {
+  target: 'https://openai.lingyu.org.cn', // 转发目标地址
+  changeOrigin: true, // 是否改变请求头的内容
+};
+
+// 创建代理中间件
+const proxy = createProxyMiddleware('/api', options);
+
+// 应用代理中间件
+app.use('/api', proxy);
+
+// 启动服务器
+app.listen(8080, () => {
+  console.log('Server is listening on port 8080');
+});
